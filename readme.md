fbbe9d03b7a02eb9d9a61e7508c2572c4fbbe592
Sergey Fogelevich <fogelevich@biptik.com>
2021-10-18 16:36:24 +1000

    init

---
+++ readme.md
@@ -0,0 +1,1 @@
+# coupon api for telegram and vk bot
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as dotenv from 'dotenv';
+
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+
+dotenv.config();
+/**
+ * App Variables
+ */
+if (!process.env.PORT) {
+  process.exit(1);
+}
+const PORT = parseInt(process.env.PORT as string, 10);
+const app = express();
+
+/**
+ *  App Configuration
+ */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Server Activation
+ */
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
